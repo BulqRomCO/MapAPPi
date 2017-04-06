41f3d8c5ec63c774511963dd18c6a96cb8ad5b07
K1532 <K1532@PROJ-422-WS-10>
2017-04-06 11:02:25 +0300

    Jees h

--- MapAPP/MainPage.xaml.cs
+++ MapAPP/MainPage.xaml.cs
@@ -411,23 +411,24 @@ namespace MapAPP
                 }
             }
         }
+        List<double> showroutebyname = new List<double>();
         private async void ReadLineData(string label)
         {
-
-            // KESKEN VITUSTIOI !!!!
+            showroutebyname.Clear();
             StorageFolder storageFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
             Debug.Write("Bussi " + label.ToString());
             string PathToGPSFile = @"Linkit\" + label + ".txt";
             StorageFile linkkitieto = await storageFolder.GetFileAsync(PathToGPSFile);
             IList<string> linjadata = await FileIO.ReadLinesAsync(linkkitieto);
             foreach (string n in linjadata)
-            {  
+            {
                 foreach (BussStops stop in stops)
                 {
                     if (n == stop.StopName)
                     {
-                        
+
                         BasicGeoposition snPosition = new BasicGeoposition() { Latitude = stop.Latitude, Longitude = stop.LonTitude };
+                        // Näytä linjan perusteella reitit 
                         Geopoint snPoint = new Geopoint(snPosition);
                         // Luodaan uusi stop 
                         MapIcon stopoint = new MapIcon();
@@ -437,10 +438,39 @@ namespace MapAPP
                         // ALLA VOIT VAIHTAA BUSSIN KUVAN
                         stopoint.Image = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/bus_stop_icon.png"));
                         JKLmap.MapElements.Add(stopoint);
+                        showroutebyname.Add(stop.Latitude);
+                        Debug.Write("Lisätään " + stop.Latitude.ToString());
+                        showroutebyname.Add(stop.LonTitude);
+                        Debug.Write("Lisätään " + stop.LonTitude.ToString());
                     }
                 }
             }
+            ShowRoutesLines(showroutebyname);
+            
+
         }
-    }
-}
+        private async void ShowRoutesLines(List<double> lista)
+        {
+
+            BasicGeoposition startPoint = new BasicGeoposition() { Latitude = lista[0], Longitude = lista[1] };
+                BasicGeoposition endPoint = new BasicGeoposition() { Latitude = lista[2], Longitude = lista[3] };
+                MapRouteFinderResult routeResult =
+                      await MapRouteFinder.GetDrivingRouteAsync(
+                      new Geopoint(startPoint),
+                      new Geopoint(endPoint),
+                      MapRouteOptimization.Time,
+                      MapRouteRestrictions.None);
+            
+                if (routeResult.Status == MapRouteFinderStatus.Success)
+                {
+                Debug.Write("Added" );
+                    MapRouteView viewOfRoute = new MapRouteView(routeResult.Route);
+                    viewOfRoute.RouteColor = Colors.ForestGreen;
+                    viewOfRoute.OutlineColor = Colors.Black;
+                    JKLmap.Routes.Add(viewOfRoute);
+            }
+            
+        }
+        }
+ }
 
